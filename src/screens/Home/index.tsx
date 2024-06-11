b09f2a4b63b64ad15836fdcc525863b537b04a15
“Thiago <thiago.osamu@outlook.com>
2024-06-10 23:53:56 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,22 @@
+import { Text, TextInput, View } from "react-native";
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 10 de junho de 2024.
+      </Text>
+
+      <TextInput 
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
+    </View>
+  )
+}
