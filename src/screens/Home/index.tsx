0004e4f0e651e4828410a17b00eb09dd85f61df2
“Thiago <thiago.osamu@outlook.com>
2024-06-11 22:37:45 -0300

    Adicionando scrollview

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,18 @@
-import { Text, TextInput, View } from "react-native";
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Participant } from "../../components/Participant";
 import { styles } from "./styles";
 
 export function Home() {
+  const participants = ['Thiago', 'Taeyang', 'Yukio', 'Baekho', 'Hanju', 'Mugyeong', 'Sartor', 'Geunbi', 'Nostrap'];
+
+  function handleParticipantAdd() {
+    console.log('Cu de telha')
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`Vc removeu o lindo ${name}`)
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -11,12 +22,30 @@ export function Home() {
       <Text style={styles.eventDate}>
         Sexta, 10 de junho de 2024.
       </Text>
+      
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
 
-      <TextInput 
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6B6B6B"
-      />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {participants.map(participant => (
+          <Participant 
+            key={participant}
+            name={participant} 
+            onRemove={() => handleParticipantRemove('Thiago')} 
+          />
+        ))}
+      </ScrollView>
     </View>
   )
 }
